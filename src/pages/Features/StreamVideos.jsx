63ef91b78461f846cd19b42898cc022b253a959f
Uttam Verma <imuv21@gmail.com>
2024-10-28 12:11:54 +0530

    initial

--- src/pages/Features/StreamVideos.jsx
+++ src/pages/Features/StreamVideos.jsx
@@ -4,7 +4,7 @@ import { Helmet } from 'react-helmet-async';
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const StreamVideo = () => {
-  
+
   const publicId = 'Ulink_Gulf1_pjc9b0';
   const videoUrl = `${BASE_URL}/service/streamVideo/${publicId}`;
 
@@ -15,9 +15,10 @@ const StreamVideo = () => {
         <meta name="description" content="One stop for everything you need on MarketPlace"></meta>
         <link rel="canonical" href="https://imuv21.netlify.app/stream-videos" />
       </Helmet>
-
-      <h1 className="text">Stream Videos</h1>
-      <video src={videoUrl} controls width="640px" height="360px" preload="auto" />
+      <div className='page flexcol g5 center wh'>
+        <h1 className="text">Stream Videos</h1>
+        <video src={videoUrl} controls className='video' preload="auto" />
+      </div>
     </div>
   );
 };
