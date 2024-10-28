f8d5a2ef195bfc4a8d4a06a22dadb250e7b52382
Uttam Verma <imuv21@gmail.com>
2024-10-28 11:56:34 +0530

    initial

--- src/pages/Features/StreamVideos.jsx
+++ src/pages/Features/StreamVideos.jsx
@@ -1,31 +1,26 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import { Helmet } from 'react-helmet-async';
-import ReactPlayer from 'react-player';
+
 const BASE_URL = import.meta.env.VITE_BASE_URL;
 
 const StreamVideo = () => {
-
-  const fileId = '1VyelNOZElir0K3dhs8yTa9pp_h2wem1x';
-  const videoUrl = `${BASE_URL}/service/streamvideo/${fileId}`;
-  const videos = [
-    "1TwGbBRigaolL5IJR8D51XUI0F7Wkr5XJ",
-    "1VyelNOZElir0K3dhs8yTa9pp_h2wem1x"
-  ];
+  
+  const publicId = 'Ulink_Gulf1_pjc9b0';
+  const videoUrl = `${BASE_URL}/service/streamVideo/${publicId}`;
 
   return (
-    <Fragment>
+    <div className='page flexcol g5 center'>
       <Helmet>
         <title>Stream Videos | MarketPlace</title>
-        <meta name="description" content="One stop for everything you need on MaarketPlace"></meta>
+        <meta name="description" content="One stop for everything you need on MarketPlace"></meta>
         <link rel="canonical" href="https://imuv21.netlify.app/stream-videos" />
       </Helmet>
-      <div className='page flexcol g5 center'>
-        <h1 className="text">Stream Videos</h1>
 
-        <ReactPlayer url={videoUrl} controls width="640px" height="360px" />
-      </div>
-    </Fragment>
+      <h1 className="text">Stream Videos</h1>
+      <video src={videoUrl} controls width="640px" height="360px" preload="auto" />
+    </div>
   );
 };
 
 export default StreamVideo;
+
